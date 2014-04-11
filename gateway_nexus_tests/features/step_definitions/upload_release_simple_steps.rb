bdfd4a03d0f8723f410b13c22e14d3753d0c540b
Matthew Willshire <matthew.willshire@surevine.com>
2014-04-11 15:40:48 +0100

    Small edits to simple upload tests

--- gateway_nexus_tests/features/step_definitions/upload_release_simple_steps.rb
+++ gateway_nexus_tests/features/step_definitions/upload_release_simple_steps.rb
@@ -8,14 +8,13 @@ Given /^a file is uploaded into the correct repository and package with a correc
 end
 
 Then /^checksums are generated and the release can be downloaded by another user with access to the relevant repository$/ do
-  # The pom.xml needs updating with correct project name, and the below links need updating
   @browser.goto("http://10.66.2.195:8081/nexus/content/repositories/snapshots/")
   @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/").click
   @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/").click
   @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/").click
-  fail "MD5 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/maven-metadata.xml.md5"
-  fail "SHA1 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/maven-metadata.xml.sha1"
+  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/").click
+  fail "MD5 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/maven-metadata.xml.md5"
+  fail "SHA1 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/maven-metadata.xml.sha1"
 end
 
 Given /^a file is uploaded into the correct repository and package with a correctly generated pom.xml file alongside a valid security marking metadata file$/ do
