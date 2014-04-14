2919d3b753ee3d899a3e5e0ee12a7b41bba61d2c
Matthew Willshire <matthew.willshire@surevine.com>
2014-04-14 16:48:31 +0100

    Adding federation tests and tweaking config

--- gateway_nexus_tests/features/step_definitions/upload_release_simple_steps.rb
+++ gateway_nexus_tests/features/step_definitions/upload_release_simple_steps.rb
@@ -1,35 +1,27 @@
 Given /^a file is uploaded into the correct repository and package with a correctly generated pom.xml file$/ do
-   visit(WelcomePage)
-   on(WelcomePage).upload_to_source "sample_matt_no_sec"
+  visit(WelcomePage)
+  on(WelcomePage).upload_to_source "sample_matt_no_sec"
 end
 
-Then /^checksums are generated and the release can be downloaded by another user with access to the relevant repository$/ do
-  @browser.goto("http://10.66.2.195:8081/nexus/content/repositories/snapshots/")
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/").click
-  fail "MD5 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/maven-metadata.xml.md5"
-  fail "SHA1 checksum not present" unless @browser.html.include? "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/maven-metadata.xml.sha1"
+Then /^checksums are generated and the release can be downloaded by another user with access to the relevant local repository$/ do
+  @browser.goto("http://10.66.2.196:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt_no_sec/2.10-SNAPSHOT/")
+  fail "MD5 checksum not present" unless @browser.html.include? "xml.md5"
+  fail "SHA1 checksum not present" unless @browser.html.include? "xml.sha1"
 end
 
 Given /^a file is uploaded into the correct repository and package with a correctly generated pom.xml file alongside a valid security marking metadata file$/ do
-  HOST = '10.66.2.196'
-  USER = 'root'
-  Net::SSH.start(HOST, USER, :keys => "~/.ssh/VPCDevInstanceDefault.pem") do|ssh|
-    @result = ssh.exec!('cd ~/sample/sample_matt && mvn deploy')
-  end
-  puts @result
+  visit(WelcomePage)
+  on(WelcomePage).upload_to_source "sample_matt"
 end
  
-Then /^checksums are generated, the security marking metadata file is present and the release can be downloaded by another user with access to the relevant repository$/ do
-  @browser.goto("http://10.66.2.195:8081/nexus/content/repositories/snapshots/")
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/").click
-  @browser.link(:href => "http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/1.2-SNAPSHOT/").click
-  fail "MD5 checksum not present" unless @browser.html.include? "xml.md5"
-  fail "SHA1 checksum not present" unless @browser.html.include? "xml.sha1"
-  fail "Security Label .xml not present" unless @browser.html.include? "securitylabel.xml"
+Then /^checksums are generated, the security marking metadata file is present and the release can be downloaded by another user with access to the relevant local and remote repositories$/ do
+  @browser.goto("http://10.66.2.196:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/2.10-SNAPSHOT/")
+  fail "MD5 checksum not present - s" unless @browser.html.include? "xml.md5"
+  fail "SHA1 checksum not present - s" unless @browser.html.include? "xml.sha1"
+  fail "Security Label .xml not present - s" unless @browser.html.include? "securitylabel.xml"
+  sleep 10
+  @browser.goto("http://10.66.2.195:8081/nexus/content/repositories/snapshots/com/surevine/tps/sample_matt/2.10-SNAPSHOT/")
+  fail "MD5 checksum not present - d" unless @browser.html.include? "xml.md5"
+  fail "SHA1 checksum not present - d" unless @browser.html.include? "xml.sha1"
+  #fail "Security Label .xml not present" unless @browser.html.include? "securitylabel.xml"
 end
--- gateway_nexus_tests/features/upload_release_simple.feature
+++ gateway_nexus_tests/features/upload_release_simple.feature
@@ -3,9 +3,9 @@ Feature: Upload a Release Using Maven
   @rm_1 @happy_path
   Scenario: Upload a simple binary file to Nexus (with no special security metadata) using the Maven “deploy:file” target.
     Given a file is uploaded into the correct repository and package with a correctly generated pom.xml file 
-	Then checksums are generated and the release can be downloaded by another user with access to the relevant repository
+	Then checksums are generated and the release can be downloaded by another user with access to the relevant local repository
 	
-  @rm_3 @happy_path @wip
+  @rm_3 @happy_path
   Scenario: Upload a simple binary file to Nexus alongside a valid security marking metadata file using the Maven “deploy:file” target.
     Given a file is uploaded into the correct repository and package with a correctly generated pom.xml file alongside a valid security marking metadata file 
-	Then checksums are generated, the security marking metadata file is present and the release can be downloaded by another user with access to the relevant repository
+	Then checksums are generated, the security marking metadata file is present and the release can be downloaded by another user with access to the relevant local and remote repositories
