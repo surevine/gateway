4c60abf3a600f2336f40d24013c85252f230d25d
Jonny Heavey <jonny.heavey@surevine.com>
2014-10-16 17:24:09 +0100

    Changed gateway to retrieve destinations from management console API rather than properties file

--- gateway-model/src/main/java/com/surevine/community/gateway/model/Destination.java
+++ gateway-model/src/main/java/com/surevine/community/gateway/model/Destination.java
@@ -3,12 +3,27 @@ package com.surevine.community.gateway.model;
 import java.net.URI;
 
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 public class Destination {
 
+	@Getter
+	private Long id;
+
+	@Getter
 	private String name;
-	private URI location;
+
+	@Getter
+	private URI url;
+
 	private boolean automatedReview;
 	private long transferDelay;
+
+	public Destination(final Long id, final String name, final URI url) {
+		this.id = id;
+		this.name = name;
+		this.url = url;
+	}
+
 }
---
+++ gateway-web/src/main/java/com/surevine/community/gateway/management/api/GatewayManagementServiceFacade.java
@@ -0,0 +1,93 @@
+package com.surevine.community.gateway.management.api;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ws.rs.ServiceUnavailableException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.surevine.community.gateway.GatewayProperties;
+import com.surevine.community.gateway.model.Destination;
+
+/**
+ * Service facade around gateway management console API
+ *
+ * @author jonnyheavey
+ *
+ */
+public class GatewayManagementServiceFacade {
+
+	private static final Logger LOG = Logger.getLogger(GatewayManagementServiceFacade.class.getName());
+
+	public static final String serviceBaseUrl = GatewayProperties.get(GatewayProperties.MANAGEMENT_CONSOLE_API_BASE_URL);
+
+	/**
+	 * Retrieve list of destinations from management console API
+	 *
+	 * @return
+	 */
+	public static List<Destination> getDestinations() {
+
+		LOG.info("Requesting destinations from management console API: " + serviceBaseUrl + "/destinations");
+
+		Client client = ClientBuilder.newClient();
+		Response response = client.target(serviceBaseUrl + "/destinations")
+								.request("application/json").get();
+
+		if(response.getStatusInfo() != Response.Status.OK) {
+			throw new ServiceUnavailableException("Unable to retrieve destinations from management console API.");
+		}
+
+		String jsonResponseBody = response.readEntity(String.class);
+		List<Destination> destinations = parseDestinationsFromResponse(jsonResponseBody);
+
+		return destinations;
+	}
+
+	/**
+	 * Parses JSON response to getDestinations request into list of Destinations
+	 *
+	 * @param responseBody String body of response
+	 * @return List of destinations
+	 * @throws URISyntaxException
+	 */
+	private static List<Destination> parseDestinationsFromResponse(String responseBody) {
+		ArrayList<Destination> destinations = new ArrayList<Destination>();
+
+		JSONArray jsonDestinations = new JSONArray(responseBody);
+
+		for (int i = 0; i < jsonDestinations.length(); i++) {
+
+			try {
+
+				JSONObject jsonDestination = jsonDestinations.getJSONObject(i);
+				Long id = jsonDestination.getLong("id");
+				String name = jsonDestination.getString("name");
+				URI url = new URI(jsonDestination.getString("url"));
+
+				Destination destination = new Destination(id, name, url);
+				destinations.add(destination);
+
+			} catch (JSONException e) {
+				LOG.warning("Unable to parse destination from JSON: " + e);
+				continue;
+			} catch (URISyntaxException e) {
+				LOG.warning("Incorrect URI syntax for destination URL: " + e);
+				continue;
+			}
+
+		}
+
+		return destinations;
+	}
+
+}
