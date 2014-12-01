6f96d0ef8dd653e92ccc70ab2ba387398d56bea8
Ash Ward <ash.wardly@gmail.com>
2014-12-01 18:43:10 +0000

    PostgreSQL install working

--- installer/install.sh
+++ installer/install.sh
@@ -9,7 +9,7 @@ WILDFLY_USER="gateway"
 POSTGRES_HOST="localhost"
 POSTGRES_DB="gatewaymanagement"
 POSTGRES_USER="console"
-POSTGRES_PASS="console"
+POSTGRES_PASS="console_password"
 
 # Adjust the below rpm paths depending on your version of CENTOS
 
@@ -22,6 +22,18 @@ LIBXML_RPM="packages/libxml2-python-2.6.26-2.1.21.el5_9.3.x86_64.rpm" # For Cent
 PYTHON_DELTA_RPM="packages/python-deltarpm-3.5-0.5.20090913git.el6.x86_64.rpm"
 LIBXML="packages/libxml2-2.7.6-14.el6_5.2.x86_64.rpm"
 
+LIBXSLT_RPM="packages/libxslt-1.1.17-4.el5_8.3.x86_64.rpm"
+POSTGRES_LIBS_RPM="packages/postgresql-libs-8.1.23-10.el5_10.x86_64.rpm"
+POSTGRES_RPM="packages/postgresql-8.1.23-10.el5_10.x86_64.rpm"
+POSTGRES_SERVER_RPM="packages/postgresql-server-8.1.23-10.el5_10.x86_64.rpm"
+#LIBXSLT_RPM="packages/libxslt-1.1.26-2.el6_3.1.x86_64"
+#POSTGRES_LIBS_RPM="packages/postgresql-libs-8.4.20-1.el6_5.x86_64.rpm"
+#POSTGRES_RPM="packages/postgresql-8.4.20-1.el6_5.x86_64.rpm"
+#POSTGRES_SERVER_RPM="packages/postgresql-server-8.4.20-1.el6_5.x86_64.rpm"
+
+PROGRESS=0
+MAX_PROGRESS=`grep -oE "^\\s*progress\\s*[0-9]*$" $BASH_SOURCE | wc -l`
+
 # Run script with umask 0022 but reset umask to original value on
 # exit, even if an error occurs during installation
 INITIAL_UMASK=`umask`
@@ -33,10 +45,18 @@ trap reset_umask EXIT
 
 clear
 
+function progress() {
+  local i=$1
+  [ "$i" == "" ] && i=1
+  PROGRESS=$((PROGRESS + i))
+  print_progress $PROGRESS
+}
+
 function print_progress() {
-  printf "%0.s#" $(seq 0 $1)
-  printf "%0.s " $(seq 0 $((50-$1)))
-  printf "  ($(($1*100/50))%%)\r"
+  printf "[" 
+  [ $1 -ne 0 ] && printf "%0.s#" $(seq 0 $1)
+  [ $1 -ne $MAX_PROGRESS ] && printf "%0.s-" $(seq 0 $(($MAX_PROGRESS-$1)))
+  printf "] ($(($1*100/MAX_PROGRESS))%%)\r"
 }
 
 echo
@@ -53,7 +73,7 @@ echo
 # Move log files aside
 cat /dev/null > $LOG_FILE
 
-print_progress 1
+progress
 touch $LOG_FILE
 
 # Header logs
@@ -69,27 +89,27 @@ else
 fi
 
 # Exract JDK
-print_progress 8
+progress
 tar xzvf "packages/jdk-7u51-linux-x64.tar.gz" -C "$INSTALL_DIR" >> $LOG_FILE
 ln -sf "$INSTALL_DIR/jdk1.7.0_51" "$INSTALL_DIR/java" >> $LOG_FILE
 
 # Extract Nexus
-print_progress 12
+progress
 tar xzvf "packages/nexus-2.7.2-03-bundle.tar.gz" -C "$INSTALL_DIR" >> $LOG_FILE
 ln -sf "$INSTALL_DIR/nexus-2.7.2-03" "$INSTALL_DIR/nexus" >> $LOG_FILE
 
 # Extract JBoss
-print_progress 16
+progress
 tar xzvf "packages/wildfly-8.0.0.Final.tar.gz" -C "$INSTALL_DIR" >> $LOG_FILE
 ln -sf "$INSTALL_DIR/wildfly-8.0.0.Final" "$INSTALL_DIR/wildfly" >> $LOG_FILE
 
 # System users
-print_progress 18
+progress
 id -u $NEXUS_USER 1>> $LOG_FILE 2>> $LOG_FILE || useradd $NEXUS_USER >> $LOG_FILE
 id -u $WILDFLY_USER 1>> $LOG_FILE 2>> $LOG_FILE || useradd $WILDFLY_USER >> $LOG_FILE
 
 # Extract Maven
-print_progress 20
+progress
 tar xzvf "packages/apache-maven-3.1.1-bin.tar.gz" -C "$INSTALL_DIR" >> $LOG_FILE
 ln -sf "$INSTALL_DIR/apache-maven-3.1.1" "$INSTALL_DIR/maven" >> $LOG_FILE
 echo 'export M2_HOME=/opt/gateway/maven' >> /etc/bashrc
@@ -100,48 +120,48 @@ export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 
 # Configure Maven
-print_progress 21
-mkdir ~gateway/.m2
+progress
+mkdir -p ~gateway/.m2
 tar xzvf packages/maven_home.tar.gz -C /home/${WILDFLY_USER}/.m2 >> $LOG_FILE
 chown -R ${WILDFLY_USER}:${WILDFLY_USER} /home/${WILDFLY_USER}/.m2 >> $LOG_FILE
 
 # Create keystore and configure Nexus' Jetty for SSL
 $INSTALL_DIR/java/jre/bin/keytool -genkey -noprompt -alias `hostname` -dname "cn=`hostname`" -storepass changeit -keyalg RSA -keystore $INSTALL_DIR/nexus/conf/keystore.jks -keysize 2048 -keypass changeit >> $LOG_FILE
 
-cp -f "nexus_jetty.xml" "$INSTALL_DIR/nexus/conf/jetty.xml" >> $LOG_FILE
+cp -f "config/nexus_jetty.xml" "$INSTALL_DIR/nexus/conf/jetty.xml" >> $LOG_FILE
 echo "application-port-ssl=8443" >> "$INSTALL_DIR/nexus/conf/nexus.properties"
 
 # NAT Nexus to port 80/443 using iptables
-print_progress 25
+progress
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8081 >> $LOG_FILE
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443 >> $LOG_FILE
 service iptables save >> $LOG_FILE
 
 # Install createrepo dependency
-print_progress 27
+progress
 IS_AMZN=`uname -a | grep amzn | wc -l`
 if [ $IS_AMZN -ne 0 ]
 then
 	yum -y install createrepo >> $LOG_FILE
 else
-	#rpm -Uvh --quiet $LIBXML || true  >> $LOG_FILE  # Centos 6 only
-        rpm -Uvh --quiet $LIBXML_RPM  || true >> $LOG_FILE
-	rpm -Uvh --quiet $CREATEREPO_RPM  || true >> $LOG_FILE
+	#rpm -Uvh --quiet $LIBXML >> $LOG_FILE 2>&1 || true  >> $LOG_FILE  # Centos 6 only
+        rpm -Uvh --quiet $LIBXML_RPM >> $LOG_FILE 2>&1  || true >> $LOG_FILE
+	rpm -Uvh --quiet $CREATEREPO_RPM >> $LOG_FILE 2>&1  || true >> $LOG_FILE
 
 fi
 
 # Customise Nexus
-print_progress 30
+progress
 cp "packages/fluent-hc-4.2.5.jar" "$INSTALL_DIR/nexus/nexus/WEB-INF/lib/" >> $LOG_FILE
 cp "packages/httpmime-4.2.5.jar" "$INSTALL_DIR/nexus/nexus/WEB-INF/lib/" >> $LOG_FILE
 cp "packages/nexus-gateway-plugin.jar" "$INSTALL_DIR/nexus/nexus/WEB-INF/lib/" >> $LOG_FILE
 
 # Add gateway war
-print_progress 31
+progress
 cp "packages/gateway.war" "$INSTALL_DIR/wildfly/standalone/deployments/" >> $LOG_FILE
 
 # Nexus service script
-print_progress 33
+progress
 ln -sf "$INSTALL_DIR/nexus/bin/nexus" "/etc/init.d/nexus" >> $LOG_FILE
 sed -i "s/#RUN_AS_USER=/RUN_AS_USER=$NEXUS_USER/g" "/etc/init.d/nexus" >> $LOG_FILE
 sed -i "12i\\\nJAVA_HOME=$INSTALL_DIR/java/jre\nPATH=\$JAVA_HOME/bin:\$PATH" "/etc/init.d/nexus" >> $LOG_FILE
@@ -151,7 +171,7 @@ chown -R $NEXUS_USER:$NEXUS_USER "$INSTALL_DIR/nexus-2.7.2-03" >> $LOG_FILE
 chown -R $NEXUS_USER:$NEXUS_USER "$INSTALL_DIR/sonatype-work" >> $LOG_FILE
 
 # JBoss service script
-print_progress 34
+progress
 ln -sf "$INSTALL_DIR/wildfly/bin/init.d/wildfly-init-redhat.sh" "/etc/init.d/wildfly" >> $LOG_FILE
 chkconfig --add wildfly >> $LOG_FILE
 chkconfig --levels 345 wildfly on >> $LOG_FILE
@@ -164,7 +184,7 @@ cp "packages/nexus-deploy.sh" $INSTALL_DIR/ >> $LOG_FILE
 # System configuration
 
 # Application configuration
-print_progress 35
+progress
 sed -i "s/jboss.bind.address:127.0.0.1}\"/jboss.bind.address:0.0.0.0}\"/g" "$INSTALL_DIR/wildfly/standalone/configuration/standalone.xml" >> $LOG_FILE
 sed -i "s/jboss.bind.address.management:127.0.0.1}\"/jboss.bind.address.management:0.0.0.0}\"/g" "$INSTALL_DIR/wildfly/standalone/configuration/standalone.xml" >> $LOG_FILE
 sed -i "s/socket-binding=.http./socket-binding=\"http\" max-post-size=\"2147483648\"/g" "$INSTALL_DIR/wildfly/standalone/configuration/standalone.xml" >> $LOG_FILE
@@ -172,6 +192,50 @@ sed -i "s/-Xmx512m/-Xmx2048m/g" "$INSTALL_DIR/wildfly/bin/standalone.conf" >> $L
 mkdir -p "$INSTALL_DIR/wildfly/modules/com/surevine/community/gateway/main" >> $LOG_FILE
 ln -sf "$INSTALL_DIR/wildfly/modules/com/surevine/community/gateway/main" "$INSTALL_DIR/config" >> $LOG_FILE
 cp -r config/* "$INSTALL_DIR/config" >> $LOG_FILE
+
+# Install and configure PostgreSQL
+date >> $LOG_FILE
+echo "Installing postgres" >> $LOG_FILE
+progress
+if rpm -q postgresql-server >> $LOG_FILE; then
+  echo "PostgreSQL Server already installed - skipping" >> $LOG_FILE
+  progress 5
+else
+  if [ $IS_AMZN -ne 0 ]
+  then
+    yum -y install postgresql-server >>$LOG_FILE
+    progress 5
+  else
+  	rpm -Uvh --quiet $LIBXSLT_RPM >>$LOG_FILE 2>&1         || true
+    progress
+    rpm -Uvh --quiet $POSTGRES_LIBS_RPM >>$LOG_FILE 2>&1   || true
+    progress 2
+    rpm -Uvh --quiet $POSTGRES_RPM >>$LOG_FILE 2>&1        || true
+    progress 2
+    rpm -Uvh --quiet $POSTGRES_SERVER_RPM >>$LOG_FILE 2>&1 || true
+  fi
+fi
+
+echo "Starting and configuring postgres" >>$LOG_FILE
+
+progress
+service postgresql restart >>$LOG_FILE 2>&1 || true
+
+progress
+echo "local $POSTGRES_DB $POSTGRES_USER md5" > /tmp/pg_hba.conf
+echo "host $POSTGRES_DB $POSTGRES_USER 127.0.0.1/32 md5" >> /tmp/pg_hba.conf
+cat /var/lib/pgsql/data/pg_hba.conf >> /tmp/pg_hba.conf
+cat /tmp/pg_hba.conf > /var/lib/pgsql/data/pg_hba.conf
+
+progress
+service postgresql restart >>$LOG_FILE 2>&1
+
+progress
+echo "Creating user and database" >>$LOG_FILE
+su - postgres -c psql >> $LOG_FILE <<SQLCommands
+CREATE USER $POSTGRES_USER PASSWORD '$POSTGRES_PASS';
+CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;
+SQLCommands
 
 # Install management console
 date >> $LOG_FILE
@@ -186,7 +250,7 @@ else
 fi
 
 # Extract management console
-print_progress 38
+progress
 unzip "packages/gateway-management.zip" -d "$CONSOLE_INSTALL_DIR" >> $LOG_FILE
 
 # Modify database connection url
@@ -199,17 +263,17 @@ export JAVA_HOME=$INSTALL_DIR/java
 CONSOLE_LOG_FILE=$CONSOLE_INSTALL_DIR/gateway-management-1.0/logs/application.log
 
 # Application startup
-print_progress 40
+progress
 service nexus start >> $LOG_FILE
-print_progress 42
+progress
 service wildfly start >> $LOG_FILE
-print_progress 44
-nohup $CONSOLE_INSTALL_DIR/gateway-management-1.0/bin/gateway-management -DapplyEvolutions.default=true -Dconfig.file=$CONSOLE_INSTALL_DIR/gateway-management-1.0/conf/application.db.conf & >> $CONSOLE_LOG_FILE
+progress
+mkdir -p `dirname $CONSOLE_LOG_FILE`
+progress
+nohup $CONSOLE_INSTALL_DIR/gateway-management-1.0/bin/gateway-management -DapplyEvolutions.default=true -Dconfig.file=$CONSOLE_INSTALL_DIR/gateway-management-1.0/conf/application.db.conf >> $CONSOLE_LOG_FILE &
 
-print_progress 46
+progress
 printf "\n"
-
-print_progress 50
 
 echo
 echo "Installation complete. Services will be started shortly."
