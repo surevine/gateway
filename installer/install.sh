29a2d3eb34cdf413123f59e1b347a8f370e30b6e
Martin Hewitt <martin.hewitt@surevine.com>
2015-01-05 16:31:23 +0000

    Removed deprecated clause

--- installer/install.sh
+++ installer/install.sh
@@ -45,22 +45,6 @@ read -p "[${INSTALL_DIR}]: " TMP_INSTALL_DIR
 if [[ ! -z "$TMP_INSTALL_DIR" ]]; then INSTALL_DIR="$TMP_INSTALL_DIR"; fi
 CONSOLE_INSTALL_DIR=${INSTALL_DIR}/console
 
-# Check if gitlab is installed - heuristic is ( user_exists('git') && contains_dir(/opt/, 'gitlab')
-GIT_USER_EXISTS="no"
-id -u git > /dev/null 2>&1 && GIT_USER_EXISTS="yes"
-
-if [ "$GIT_USER_EXISTS" == "no" ]; then
-    echo "No git user found, please install git and Gitlab"
-    exit 1;
-fi
-
-GITLAB_VERSION=$(ls /opt/ | grep -v grep | grep -i gitlab-7)
-
-if [[ -z "$GITLAB_VERSION" ]]; then
-    echo "No GitLab 7 install detected in /opt/"
-    exit 1;
-fi
-
 echo "Have you installed Gitlab version 7.4 or greater?"
 read -p "[Y/n]: " GITLAB_INSTALLED
 if [[ -z "$GITLAB_INSTALLED" ]]; then GITLAB_INSTALLED="Y"; fi
