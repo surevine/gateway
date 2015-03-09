c79480d9c45cd956f2b775c98e5cd2f1b6fef96e
Dan Hicks <dan.hicks@surevine.com>
2015-03-09 12:14:41 +0000

    Added robustness to modules/scripts template updating as part of deployment. Will now not try and loop through an empty argument list.

--- wildfly-packager/src/main/rar/updateTemplateModules.sh
+++ wildfly-packager/src/main/rar/updateTemplateModules.sh
@@ -37,30 +37,32 @@ cp -r "${TEMPLATE_DIRECTORY}" "${TARGET_DIRECTORY}"
 for TARGET_FILE in `find ${TARGET_DIRECTORY} -name "*.properties"`; do
 
   # For each file we process, loop through the source configuration files given to the script
-  for REPLACEMENT_FILE in "$@"; do
+  if [ ! -z "$@" ]; then
+    for REPLACEMENT_FILE in "$@"; do
 
-    # Split each line of the source files into the key and value components as separated by the =
-    while IFS="=" read -r KEY VALUE; do
+      # Split each line of the source files into the key and value components as separated by the =
+      while IFS="=" read -r KEY VALUE; do
 
-      # Replace '.' in the key with '\.' to ensure they are escaped in the sed regex replacements
-      KEY=`echo "${KEY}" | sed "s/\./\\\./g"`
+        # Replace '.' in the key with '\.' to ensure they are escaped in the sed regex replacements
+        KEY=`echo "${KEY}" | sed "s/\./\\\./g"`
 
-      # Replace '.', ' ' and '/' in the value to ensure they are appropriately escaped in the sed regex replacements
-      VALUE=`echo "${VALUE}" | sed "s/\./\\\./g" | sed 's/[ ]/WHITESPACE/g' | sed "s/\//FORWARDSLASH/g"`
+        # Replace '.', ' ' and '/' in the value to ensure they are appropriately escaped in the sed regex replacements
+        VALUE=`echo "${VALUE}" | sed "s/\./\\\./g" | sed 's/[ ]/WHITESPACE/g' | sed "s/\//FORWARDSLASH/g"`
 
-      # Only process if we have a key (skips blank lines)
-      if [ ! -z "${KEY}" ]; then
+        # Only process if we have a key (skips blank lines)
+        if [ ! -z "${KEY}" ]; then
 
-        # Perform the replacement to put the new value in
-        # Will only match when the key is at the start of the line - will skip commented configuration
-        sed -i -e "s/^${KEY}=.*/${KEY}=${VALUE}/g" ${TARGET_FILE}
+          # Perform the replacement to put the new value in
+          # Will only match when the key is at the start of the line - will skip commented configuration
+          sed -i -e "s/^${KEY}=.*/${KEY}=${VALUE}/g" ${TARGET_FILE}
 
-        # Reverse the previous escape replacements
-        sed -i -e "s/WHITESPACE/ /g" ${TARGET_FILE}
-        sed -i -e "s/FORWARDSLASH/\//g" ${TARGET_FILE}
-      fi
-    done < "${REPLACEMENT_FILE}"
-  done
+          # Reverse the previous escape replacements
+          sed -i -e "s/WHITESPACE/ /g" ${TARGET_FILE}
+          sed -i -e "s/FORWARDSLASH/\//g" ${TARGET_FILE}
+        fi
+      done < "${REPLACEMENT_FILE}"
+    done
+  fi
 done
 
 printFinish
